37baf98cdb428bcee13aba12e0ed0b147ba63d7c
htmlpluscss <79198889134@ya.ru>
2019-10-22 12:14:13 +0300

    bg-footer

---
+++ src/js/bg-body.js
@@ -0,0 +1,27 @@
+(function(footer){
+
+	if ('IntersectionObserver' in window) {
+
+		var options = {
+			root: null,
+			rootMargin: '0px',
+			threshold: [0.1]
+		};
+
+		var callback = function(entries, observer) {
+
+			Array.prototype.forEach.call(entries, function(entry){
+
+				document.body.classList.toggle('bg-footer', entry.intersectionRatio >= 0.1);
+
+			});
+
+		};
+
+		var observer = new IntersectionObserver(callback, options);
+
+		observer.observe(footer);
+
+	}
+
+})(document.querySelector('.footer'));
